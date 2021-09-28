d6c6e9043158ac85d7101897ce0268c7c13bf3ab
Samu Garron <garron.garron@gmail.com>
2021-09-28 11:02:31 -0300

    Array.toString()

--- js/Component.js
+++ js/Component.js
@@ -1,20 +1,20 @@
 class Component {
-    constructor(){
+    constructor() {
         this.state = {}
         this.prop = {}
         this.node = null
         this.done = null
     }
-    setState(){
+    setState() {
         return {}
     }
-    setDefaultState(){
+    setDefaultState() {
         this.state = this.setState()
     }
     addEventListener() {
         return [];
     }
-    settingListener(parent){
+    settingListener(parent) {
         this.addEventListener().map(selector => {
             let nodeList = parent.querySelectorAll('[' + selector + ']')
             for (let index = 0; index < nodeList.length; index++) {
@@ -27,14 +27,14 @@ class Component {
     setChildComponent() {
         return [];
     }
-    setProperties(node, instanceComponet){
+    setProperties(node, instanceComponet) {
         for (let index = 0; index < node.attributes.length; index++) {
-            if(typeof this[node.attributes[index].value] == 'function'){
+            if (typeof this[node.attributes[index].value] == 'function') {
                 instanceComponet.prop[node.attributes[index].name] = this[node.attributes[index].value]()
             }
         }
     }
-    addChild(parent){
+    addChild(parent) {
         this.setChildComponent().map(subComponent => {
             let nodeList = parent.querySelectorAll(subComponent.name)
             for (let index = 0; index < nodeList.length; index++) {
@@ -47,33 +47,42 @@ class Component {
         })
         return parent
     }
-    map(propName, callback){
-        if(Array.isArray(this.prop[propName]))
+    map(propName, callback) {
+        if (Array.isArray(this.prop[propName]))
             return this.prop[propName].map(callback).join('')
+        console.error(`Property ${propName} is not Array`);
         throw `Property ${propName} is not Array`
     }
-    setNewState(state){
+    setNewState(state) {
         this.state = state
-        console.log(this.state);
         let old = this.node
         this.node.parentNode.replaceChild(this.exec(true), old)//?
         this.afterUpdate(this.node.parentNode)
     }
-    beforeAppendChild(){
+    beforeAppendChild() {
 
     }
-    afterUpdate(){
+    afterUpdate() {
 
     }
-    template(){
+    template() {
         throw "No template"
     }
+    propMap(callback) {
+        Object.keys(this.prop).forEach(keys => {
+            if (Array.isArray(this.prop[keys]))
+                this.prop[keys].__proto__.toString = function () {
+                    return this.join('');
+                };
+        })
+        return this.prop
+    }
     exec(update = false) {
-        if(!update) this.setDefaultState()
-        let node = new DOMParser().parseFromString(this.template(), 'text/html')
+        if (!update) this.setDefaultState()
+        let node = new DOMParser().parseFromString(this.template(this.propMap()), 'text/html')
         this.settingListener(node.body)
         this.addChild(node.body)
-        if(!this.done){
+        if (!this.done) {
             this.done = true
             this.beforeAppendChild(node.body)
         }
@@ -81,7 +90,7 @@ class Component {
         this.node = out
         return out
     }
-    querySelector(selector){
+    querySelector(selector) {
         document.querySelector(selector).appendChild(this.exec())
     }
 }
